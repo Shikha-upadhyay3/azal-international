f5da5e0bf9bac74da2d27d8c6490996aed36b057
Shikha-upadhyay3 <sikhaupadhya1@gmail.com>
2026-02-07 12:19:37 +0530

    Day 2: Final path and case sensitivity fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MainLayout from './layout/MainLayout';
-import Home from './pages/Home';
+import MainLayout from './layout/MainLayout'; // 'l' chota rakhein
+import Home from './pages/Home';              // 'p' chota rakhein
 import AboutUs from './pages/AboutUs'; 
-// Baki saare pages bhi import karein (Collections, Projects, etc.) [cite: 23-31]
+import Collections from './pages/Collections';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* MainLayout ke andar saare pages aayenge [cite: 15-20] */}
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<AboutUs />} />
-          {/* Day 2 ke baki routes yanha add karein */}
+          <Route path="collections" element={<Collections />} />
+          <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
     </Router>
